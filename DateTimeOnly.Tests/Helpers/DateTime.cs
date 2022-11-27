31edc0da2ff3edc606bc9a8a132fb983f97e9427
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2022-11-27 09:04:20 +0100

    Backported unit tests from the .NET 7.0 release branch.

---
+++ DateTimeOnly.Tests/Helpers/DateTime.cs
@@ -0,0 +1,20 @@
+﻿// ReSharper disable once CheckNamespace
+namespace System.Tests
+{
+    internal static class DateTimeExtensions
+    {
+        internal static System.DateTime Create(int year, int month, int day, int hour, int minute, int second, int millisecond, int microsecond) =>
+            new System.DateTime(year, month, day, hour, minute, second, millisecond)
+                .Add(new TimeSpan(microsecond * Diagnostics.TimeSpan.TicksPerMicrosecond));
+
+        /// <summary>
+        /// The microseconds component, expressed as a value between 0 and 999.
+        /// </summary>
+        internal static int Microsecond(this System.DateTime dateTime) => (int)((dateTime.Ticks / Diagnostics.TimeSpan.TicksPerMicrosecond) % 1000);
+
+        /// <summary>
+        /// The nanoseconds component, expressed as a value between 0 and 900 (in increments of 100 nanoseconds).
+        /// </summary>
+        internal static int Nanosecond(this System.DateTime dateTime) => (int)(dateTime.Ticks % Diagnostics.TimeSpan.TicksPerMicrosecond) * 100;
+    }
+}
---
+++ DateTimeOnly.Tests/Helpers/TimeSpan.cs
@@ -0,0 +1,10 @@
+﻿// ReSharper disable once CheckNamespace
+
+namespace System.Tests.Diagnostics
+{
+    internal static class TimeSpan
+    {
+        internal const long TicksPerMicrosecond = 10;
+        internal const long NanosecondsPerTick = 100;
+    }
+}
