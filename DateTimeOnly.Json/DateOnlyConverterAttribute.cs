97eaaf2d22ef1b11139e687a16a9e0b2bbe96106
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2025-02-17 13:15:47 +0100

    The compatibility checks were fixed, and condition compilation was upgraded.

--- DateTimeOnly.Json/DateOnlyConverterAttribute.cs
+++ DateTimeOnly.Json/DateOnlyConverterAttribute.cs
@@ -1,11 +1,11 @@
 ﻿using System.Text.Json.Serialization;
-#if NET6_0_OR_GREATER
+#if NET8_0_OR_GREATER
 using System.Text.Json.Serialization.Metadata;
 #endif
 
 namespace System.Text.Json;
 
-#if NET6_0_OR_GREATER
+#if NET8_0_OR_GREATER
 /// <summary>
 /// Attribute for handling the <see cref="DateOnly"/> data type with the <see href="https://docs.microsoft.com/dotnet/api/system.text.json">System.Text.Json</see> library.
 /// At run-time selecting the built-in <see cref="JsonMetadataServices.DateOnlyConverter"/> converter using the converters factory method.
@@ -23,7 +23,7 @@ public sealed class DateOnlyConverterAttribute : JsonConverterAttribute
     public override JsonConverter CreateConverter(
         Type typeToConvert)
     {
-#if NET6_0_OR_GREATER
+#if NET8_0_OR_GREATER
         return JsonMetadataServices.DateOnlyConverter;
 #else
         return new DateOnlyConverter();
--- DateTimeOnly.Json/Helpers/NullableAttributes.cs
+++ DateTimeOnly.Json/Helpers/NullableAttributes.cs
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#if NETSTANDARD
+#if NETSTANDARD2_0
 
 // ReSharper disable once CheckNamespace
 namespace System.Diagnostics.CodeAnalysis;
